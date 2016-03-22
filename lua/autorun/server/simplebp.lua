4103eb1166bfc99c6af4e2b67f7341a53313d3d5
redpr1sm <fbf375bf@opayq.com>
2016-03-22 18:07:43 -0500

    ready for master

--- lua/autorun/server/simplebp.lua
+++ lua/autorun/server/simplebp.lua
@@ -1,43 +1,46 @@
-if not SERVER then return end
-print("SimpleBP by redpr1sm is running on this server!")
+print("#-----------------------------------------------#")
+print("|SimpleBP by redpr1sm is running on this server!|")
+print("#-----------------------------------------------#")
 
-function spawnProtect( ply )
-    if IsValid( ply ) and not ply:HasGodMode() then
-        ply:GodEnable()
-        ply:ChatPrint("You now have build protection. (Spawned)")
-    end
-end
-hook.Add("PlayerSpawn","",spawnProtect)
+if SERVER then
+	function spawnProtect( ply )
+		if IsValid( ply ) and not ply:HasGodMode() then
+			ply:GodEnable()
+			ply:ChatPrint("You now have build protection. (Spawned)")
+		end
+	end
+	hook.Add("PlayerSpawn","",spawnProtect)
 
-function tableContains( target, element )
-    for K,V in pairs(target) do
-        if V==element then
-            return true
-        end
-    end
-    return false
-end
+	function tableContains( target, element )
+		for K,V in pairs(target) do
+			if V==element then
+				return true
+			end
+		end
+		return false
+	end
 
-Safe = { "weapon_physgun", "gmod_tool", "gmod_camera" }
-function badWeapon( ply, old, new )
-    if IsValid( ply ) and IsValid( new ) and not tableContains( Safe, new:GetClass() ) then
-        ply:GodDisable()
-        ply:ChatPrint("You've lost build protection! (Equipped weapon)")
-    end
-end
-hook.Add("PlayerSwitchWeapon","",badWeapon)
+	Safe = { "weapon_physgun", "gmod_tool", "gmod_camera" }
+	function badWeapon( ply, old, new )
+		if IsValid( ply ) and IsValid( new ) and not tableContains( Safe, new:GetClass() ) then
+			ply:GodDisable()
+			ply:ChatPrint("You've lost build protection! (Equipped weapon)")
+		end
+	end
+	hook.Add("PlayerSwitchWeapon","",badWeapon)
 
-function enterVehicle( ply, veh )
-    if IsValid( ply ) and IsValid( veh ) then
-        ply:GodDisable()
-        ply:ChatPrint("You've lost build protection! (Entered vehicle)")
-    end
-end
-hook.Add("PlayerEnteredVehicle","",enterVehicle)
+	function enterVehicle( ply, veh )
+		if IsValid( ply ) and IsValid( veh ) then
+			ply:GodDisable()
+			ply:ChatPrint("You've lost build protection! (Entered vehicle)")
+		end
+	end
+	hook.Add( "PlayerEnteredVehicle", "", enterVehicle )
 
-function findPlayers()
-    T=nil
-    T=ents.FindByClass( "player" )
+	function findPlayers()
+		T=nil
+		T=ents.FindByClass( "player" )
+	end
+	hook.Add( "PlayerConnect","",findPlayers )
+	hook.Add( "PlayerDisconnected", "", findPlayers )
 end
-hook.Add("PlayerConnect","",findPlayers)
-hook.Add("PlayerDisconnected","",findPlayers)
