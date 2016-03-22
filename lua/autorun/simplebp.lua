e8bfded4e823dfb72021dd53f599de0f9695ee46
redpr1sm <3ef4c1bb@opayq.com>
2016-03-22 17:07:30 -0500

    Update simplebp.lua

--- lua/autorun/simplebp.lua
+++ lua/autorun/simplebp.lua
@@ -1,3 +1,4 @@
+if not SERVER then return end
 function spawnProtect( ply )
     if IsValid( ply ) and not ply:HasGodMode() then
         ply:GodEnable()
@@ -6,13 +7,26 @@ function spawnProtect( ply )
 end
 hook.add("PlayerSpawn","",spawnProtect)
 
-function tableContains( element )
-    
+function tableContains( target, element )
+    for K,V in pairs(target) do
+        if V==element then
+            return true
+        end
+    end
+    return false
 end
 
 Safe = { "weapon_physgun", "gmod_tool", "gmod_camera" }
 function badWeapon( ply, old, new )
-    if IsValid( ply ) and not Safe:tableContains( new:GetClass() ) then
-        
+    if IsValid( ply ) and not tableContains( Safe, new:GetClass() ) then
+        ply:GodDisable()
+        ply:ChatPrint("You've lost build protection!")
+    end
+end
+
+function inVehicle( ply, veh )
+    if IsValid( ply ) and IsValid( veh ) then
+        ply:GodDisable()
+        ply:ChatPrint("You've lost build protection!")
     end
 end
