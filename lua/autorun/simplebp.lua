1bbc148711471b84da8b77cae9b6551b41cc49ff
redpr1sm <3ef4c1bb@opayq.com>
2016-03-23 19:39:57 -0500

    Update simplebp.lua

--- lua/autorun/simplebp.lua
+++ lua/autorun/simplebp.lua
@@ -46,7 +46,7 @@ if CLIENT then
 		texture = surface.GetTextureID( "materials/shield/shield" ),
 		color   = Color( 255,255,255,255 ),
 		x       = SX/2-32,
-		y       = SY,
+		y       = 64,
 		w       = 32,
 		h       = 32
 	}
@@ -56,7 +56,7 @@ if CLIENT then
 			draw.Text( {
 				text   = "You have build protection.",
 				font   = "HudHintTextLarge",
-				pos    = {SX/2, SY},
+				pos    = {SX/2-40, 64},
 				xalign = TEXT_ALIGN_CENTER,
 				yalign = TEXT_ALIGN_CENTER,
 				color  = color_white
